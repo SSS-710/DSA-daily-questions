ee88fbca20b02bf4915b2f1f4452707561542969
Shashank Singh Sikarwar <shashanksss710@gmail.com>
2026-07-05 23:20:43 +0530

    more questions

---
+++ even.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int findNumbers(vector<int>& nums) {
+
+        int count = 0;
+
+        for(int num : nums)
+        {
+            int digits = 0;
+
+            while(num > 0)
+            {
+                digits++;
+                num /= 10;
+            }
+
+            if(digits % 2 == 0)
+            {
+                count++;
+            }
+        }
+
+        return count;
+    }
+};
